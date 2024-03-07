bcb20cb95a1c0f73e4915566af22b5c028f2db93
funny-lee <1750285541@qq.com>
2024-03-08 01:08:32 +0800

    add rime-dict

--- rime/default.nix
+++ rime/default.nix
@@ -1,3 +1,11 @@
-{lib, ...}: {
+{lib,  catppuccin-fcitx5,...}: {
+
   home.file.".local/share/fcitx5/rime/default.custom.yaml".source = ./default.custom.yaml;
+  home.file.".local/share/fcitx5/themes".source = "${catppuccin-fcitx5}/src";
+  home.file.".config/fcitx5/conf/classicui.conf".source = ./classicui.conf;
+  home.file.".config/fcitx5/rime" = {
+    source = ./rime-dict;
+    recursive = true;   # 递归整个文件夹
+    executable = true;  # 将其中所有文件添加「执行」权限
+  };
 }
