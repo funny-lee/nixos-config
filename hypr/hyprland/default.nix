f8a4b5f34d88e320213c27df76da0c99430e9d3e
funny-lee <1750285541@qq.com>
2024-04-03 02:41:45 +0800

    nvim

--- hypr/hyprland/default.nix
+++ hypr/hyprland/default.nix
@@ -1,50 +1,72 @@
 {
   pkgs,
+  lib,
   hyprland,
-  catppuccin-hyprland,
+  nur-ryan4yin,
   ...
 }: {
-  imports = [
-    # hyprland.homeManagerModules.default
+imports =[
+./anyrun.nix
+./nvidia.nix
+./wayland-apps.nix
 
-    ./anyrun.nix
-    ./wayland-apps.nix
-  ];
+];
+  # NOTE:
+  # We have to enable hyprland/i3's systemd user service in home-manager,
+  # so that gammastep/wallpaper-switcher's user service can be start correctly!
+  # they are all depending on hyprland/i3's user graphical-session
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      source = "${nur-ryan4yin.packages.${pkgs.system}.catppuccin-hyprland}/themes/mocha.conf";
+      env = [
+        "NIXOS_OZONE_WL,1" # for any ozone-based browser & electron apps to run on wayland
+        "MOZ_ENABLE_WAYLAND,1" # for firefox to run on wayland
+        "MOZ_WEBRENDER,1"
+        # misc
+        "_JAVA_AWT_WM_NONREPARENTING,1"
+        "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
+        "QT_QPA_PLATFORM,wayland"
+        "SDL_VIDEODRIVER,wayland"
+        "GDK_BACKEND,wayland"
+      ];
+    };
+    package = hyprland.packages.${pkgs.system}.hyprland;
+    extraConfig = builtins.readFile ./hypr-conf/hyprland.conf;
+    # gammastep/wallpaper-switcher need this to be enabled.
+    systemd.enable = true;
+  };
 
-  # wayland.windowManager.hyprland.enable = true;
+  # NOTE: this executable is used by greetd to start a wayland session when system boot up
+  # with such a vendor-no-locking script, we can switch to another wayland compositor without modifying greetd's config in NixOS module
+  home.file.".wayland-session" = {
+    source = "${pkgs.hyprland}/bin/Hyprland";
+    executable = true;
+  };
 
   # hyprland configs, based on https://github.com/notwidow/hyprland
-  home.file.".config/hypr" = {
-    source = ./hypr-conf;
-    # copy the scripts directory recursively
-    recursive = true;
-  };
-  home.file.".config/hypr/themes".source = "${catppuccin-hyprland}/themes";
+  xdg.configFile = {
+    "hypr/mako" = {
+      source = ../conf/mako;
+      recursive = true;
+    };
+    "hypr/scripts" = {
+      source = ../conf/scripts;
+      recursive = true;
+    };
+    "hypr/waybar" = {
+      source = ../conf/waybar;
+      recursive = true;
+    };
+    "hypr/wlogout" = {
+      source = ../conf/wlogout;
+      recursive = true;
+    };
 
-  # music player - mpd
-  home.file.".config/mpd" = {
-    source = ./mpd;
-    recursive = true;
-  };
-  home.file.".config/zathura" = {
-    source = ./zathura;
-    recursive = true;
-  };
-
-  # allow fontconfig to discover fonts and configurations installed through home.packages
-  fonts.fontconfig.enable = true;
-
-  systemd.user.sessionVariables = {
-    "NIXOS_OZONE_WL" = "1"; # for any ozone-based browser & electron apps to run on wayland
-    "MOZ_ENABLE_WAYLAND" = "1"; # for firefox to run on wayland
-    "MOZ_WEBRENDER" = "1";
-
-    # for hyprland with nvidia gpu, ref https://wiki.hyprland.org/Nvidia/
-    "LIBVA_DRIVER_NAME" = "nvidia";
-    "XDG_SESSION_TYPE" = "wayland";
-    "GBM_BACKEND" = "nvidia-drm";
-    "__GLX_VENDOR_LIBRARY_NAME" = "nvidia";
-    "WLR_NO_HARDWARE_CURSORS" = "1";
-    "WLR_EGL_NO_MODIFIRES" = "1";
+    # music player - mpd
+    "mpd" = {
+      source = ../conf/mpd;
+      recursive = true;
+    };
   };
 }
