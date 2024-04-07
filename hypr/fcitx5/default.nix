06b65aa843862e2906c41ed1dc4ccd4b1062b7dc
funny-lee <1750285541@qq.com>
2024-04-07 20:52:56 +0800

    nvim

--- hypr/fcitx5/default.nix
+++ hypr/fcitx5/default.nix
@@ -11,7 +11,6 @@
   home.file.".local/share/fcitx5/themes".source = "${catppuccin-fcitx5}/src";
   home.file.".config/fcitx5/conf/classicui.conf".source = ./classicui.conf;
 
-  home.file.".local/share/fcitx5/rime/default.custom.yaml".source = ./default.custom.yaml;
   # every time fcitx5 switch input method, it will modify ~/.config/fcitx5/profile file,
   # which will override my config managed by home-manager
   # so we need to remove it before everytime we rebuild the config
